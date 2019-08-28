a4bd9658002a6fc5a8f385b864d736b9c0b7a1a2
AceofGrades <47581354+AceofGrades@users.noreply.github.com>
2019-08-28 13:49:58 +1000

    Added scripts and assembly definitions

---
+++ Assets/UnitTests/Scripts/BombermanTests.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.TestTools;
+using NUnit.Framework;
+
+public class BombermanTests
+{
+    private GameObject game; // Stores Instance of Entire Game
+    private Player player1, player2;
+
+    [SetUp]
+    void SetUp()
+    {
+        GameObject gamePrefab = Resources.Load<GameObject>("Prefabs/Game");
+        game = Object.Instantiate(gamePrefab);
+
+        Player[] players = Object.FindObjectsOfType<Player>();
+
+        foreach(var player in players)
+        {
+            if(player.playerNumber == 1)
+            {
+                player1 = player;
+            }
+            else if(player.playerNumber == 2)
+            {
+                player2 = player;
+            }
+        }
+    }
+
+    [TearDown]
+    void TearDown()
+    {
+
+    }
+}
