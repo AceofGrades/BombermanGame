b872d8ad5fcbf262869a844ee85d369fe37d4c4e
AceofGrades <47581354+AceofGrades@users.noreply.github.com>
2019-08-28 16:04:39 +1000

    Added tests and updated player movement and bomb placement

--- Assets/UnitTests/Scripts/BombermanTests.cs
+++ Assets/UnitTests/Scripts/BombermanTests.cs
@@ -8,32 +8,89 @@ using NUnit.Framework;
 public class BombermanTests
 {
     private GameObject game; // Stores Instance of Entire Game
-    private Player player1, player2;
+    private Player[] players;
 
+    // Method for getting reference to player by index
+    public Player GetPlayer(int index)
+    {
+        // Loops through all players from SetUp function
+        foreach (var player in players)
+        {
+            // Compares the playerNumber with given index
+            if(player.playerNumber == index)
+            {
+                // Returns that player
+                return player;
+            }
+        }
+        // All else fails, return null
+        return null;
+    }
     [SetUp]
-    void SetUp()
+    public void SetUp()
     {
         GameObject gamePrefab = Resources.Load<GameObject>("Prefabs/Game");
         game = Object.Instantiate(gamePrefab);
+        players = Object.FindObjectsOfType<Player>();
+    }
 
-        Player[] players = Object.FindObjectsOfType<Player>();
+    // >> TESTS GO HERE <<
+    [UnityTest]
+    public IEnumerator Player1DropsBomb()
+    {
+        Player player1 = GetPlayer(1);
 
-        foreach(var player in players)
-        {
-            if(player.playerNumber == 1)
-            {
-                player1 = player;
-            }
-            else if(player.playerNumber == 2)
-            {
-                player2 = player;
-            }
-        }
+        // Simulate Bomb Dropping
+        player1.DropBomb();
+
+        // Wait for last frame
+        yield return new WaitForEndOfFrame();
+
+        // Check if Bomb Exists in the Scene
+        Bomb bomb = Object.FindObjectOfType<Bomb>();
+
+        // Bomb is not null
+        Assert.IsTrue(bomb != null, "The Bomb didn't spawn");
+    }
+
+    [UnityTest]
+    public IEnumerator Player2DropsBomb()
+    {
+        Player player2 = GetPlayer(2);
+
+        // Simulate Bomb Dropping
+        player2.DropBomb();
+
+        // Wait for last frame
+        yield return new WaitForEndOfFrame();
+
+        // Check if Bomb Exists in the Scene
+        Bomb bomb = Object.FindObjectOfType<Bomb>();
+
+        // Bomb is not null
+        Assert.IsTrue(bomb != null, "The Bomb didn't spawn");
+    }
+
+    [UnityTest]
+    public IEnumerator Player1Moves()
+    {
+        Player player1 = GetPlayer(1);
+
+        Vector3 oldPosition = player1.transform.position;
+
+        player1.Move(true, false, false, false);
+
+        yield return new WaitForFixedUpdate();
+
+        Vector3 newPosition = player1.transform.position;
+
+        Assert.IsTrue(oldPosition !=newPosition);
     }
 
     [TearDown]
-    void TearDown()
+    public void TearDown()
     {
-
+        // Remove the Game from the Scene
+        Object.Destroy(game);
     }
 }
